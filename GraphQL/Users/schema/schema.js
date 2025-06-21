98892497e55a6a64cc73f8dc3613495e2a9b1df9
Divya-Konala <divyakonala99@gmail.com>
2025-06-21 23:07:01 +0530

    queries & nested queries

--- GraphQL/Users/schema/schema.js
+++ GraphQL/Users/schema/schema.js
@@ -1,39 +1,47 @@
-const graphQL = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql");
+const axios = require('axios');
 const _ = require('lodash');
-const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema,
-} = graphQL;
 
-const users = [
-    {id:'23', firstName: 'Bill', age: 21 },
-    {id:'46', firstName: 'Samantha', age: 22},
-]
-
-const UserType = new GraphQLObjectType ({
-        name: 'User',
-        fields: {
-            id: {type: GraphQLString},
-            firstName: {type: GraphQLString},
-            age: {type: GraphQLInt},
-        }
+const CompanyType = new GraphQLObjectType({
+    name: "Company",
+    fields: {
+        id : {type: GraphQLString},
+        name : {type: GraphQLString},
+        description : {type: GraphQLString},
+    }
 });
 
-const RootQuery = new GraphQLObjectType ({
-    name: 'RootQueryType',
+// nested queries
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: {
+        id : {type: GraphQLString},
+        firstName : {type: GraphQLString},
+        age : {type: GraphQLInt},
+        company : {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                .then((res)=>res.data)
+            }
+        }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
     fields: {
         user: {
             type: UserType,
-            args: { id: { type: GraphQLString } },
+            args: {id: {type: GraphQLString}},
             resolve(parentValue, args) {
-                return _.find(users, {id: args.id});
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                .then((res) => res.data);
             }
-        },
+        }
     }
 })
 
-module.exports = new GraphQLSchema ({
+module.exports = new GraphQLSchema({
     query: RootQuery
-});
+})
